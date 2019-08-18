0905f2ce1c5d7f69e1c0949f2b9b949ce084f403
Tim Knoll <timothy.knoll@gmail.com>
2019-08-18 21:57:35 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,9 +23,9 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.find_or_create_by(attributes)
 end
 
-def can_be_created_in_a_block (title: "The Room", release_date:)
+def can_be_created_in_a_block (title: , release_date:)
     Movie.create do |m|
-      m.title = "Home Alone"
+      m.title 
       m.release_date
     end
 end
